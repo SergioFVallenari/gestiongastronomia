25c530f88f3914b620eca8e5e176e60fc46fe528
Sergio <vallenarisergio@gmail.com>
2025-03-02 13:26:13 -0300

    fix

--- backend/src/controllers/ventas.ts
+++ backend/src/controllers/ventas.ts
@@ -1,11 +1,17 @@
 import { Request, Response, Router } from "express";
 const app = Router();
 import classVentas from '../class/class_ventas'
+import { decodeToken } from "../helpers";
 const  {insertVenta, manejoStock,getVentas} = new classVentas()
 
 app.post('/insert_venta', async (req: Request, res: Response) => {
     const body = req.body
     try {
+        const decoded = decodeToken(req.query.token as string)
+        console.log(decoded)
+        if (typeof decoded !== 'string' && decoded?.id) {
+            body.id = decoded.id;
+        }
         const response:any = await insertVenta(body)
         await manejoStock(body, response[0]?.idventa)
         res.status(200).json({
